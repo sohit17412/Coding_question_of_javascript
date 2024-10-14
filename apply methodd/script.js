811659b62de081ae49b65adcaa4913270a578502
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-10-14 10:51:04 +0530

    Update script.js

--- apply methodd/script.js
+++ apply methodd/script.js
@@ -1,5 +1,6 @@
 const ob1 = {
-  fullname: function () { // important :-- when ever you use arrow funtion you cant access the this keyword.
+  fullname: function () {
+    // important :-- when ever you use arrow funtion you cant access the this keyword.
     console.log(`Developer name is ${this.first} ${this.last}`);
   },
 };
@@ -10,3 +11,17 @@ const ob2 = {
 };
 
 ob1.fullname.apply(ob2);
+
+/***********argumented */
+
+const describe = {
+  fun: function (city, job) {
+    console.log(
+      `${this.first} ${this.last} lives in ${city} and he is a ${job}`
+    );
+  },
+};
+
+describe.fun.apply(ob2, ["Lucknow", "Software developer"]);
+
+/**same works with the call function the only differnce is it takes arguments in simple for example ;;-- describe.fun.call(ob2,'Lucknow','software developer') */
